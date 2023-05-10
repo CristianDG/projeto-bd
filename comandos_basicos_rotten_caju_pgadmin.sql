1607a584e14d9dac94a8c2917060d59bfd331652
Arthur Henrique Silva de Lima Arthur <115894179+ArthurHenriqueSilva@users.noreply.github.com>
2023-05-10 15:16:03 -0300

    Contém todos os selects do banco.

---
+++ comandos_basicos_rotten_caju_pgadmin.sql
@@ -0,0 +1,15 @@
+set search_path to public
+
+
+select * from associacao_criticos;
+select * from cargo;
+select * from critica;
+select * from criticas__obras;
+select * from filme;
+select * from genero;
+select * from obra;
+select * from premio;
+select * from produtora;
+select * from serie;
+select * from staff;
+select * from usuario;
